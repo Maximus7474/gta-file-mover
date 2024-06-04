c579f574dd064d43998054fa3146985047e24dc1
Maximus7474 <94017712+Maximus7474@users.noreply.github.com>
2024-06-04 21:03:35 +0200

    feat: main batch file

---
+++ file-mover.bat
@@ -0,0 +1,36 @@
+@echo off
+
+:: BatchGotAdmin code start
+REM --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin access
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"="
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+:: BatchGotAdmin code end
+
+set "source=D:\Path\To\Modded\Files"
+set "destination=E:\Path\To\Grand Theft Auto V"
+
+echo Copying folder...
+robocopy "%source%" "%destination%" /E /COPY:DAT /IS /R:0 /W:0 /V
+
+echo All folders copied successfully.
+timeout /t 30 >nul
